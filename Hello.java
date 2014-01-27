b800c805f34a76060b3ba6cb50c19aa2681c7591
Stephanie Zhu <stzhu@vassar.edu>
2014-01-27 16:53:20 -0500

    First snapshot

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello
+{
+  public static String sayHello()
+  {
+    return "Hello world";
+  }
+  
+  public static void main(String[] args)
+  {
+    System.out.println(sayHello());
+  }
+}
